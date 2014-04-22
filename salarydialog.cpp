1ee1d8af7457bc70cac3dce39f1f76fffe00a95a
neverlandgy <neverlandgy@gmail.com>
2014-04-22 15:26:58 +0800

    完成发放工资模块

--- salarydialog.cpp
+++ salarydialog.cpp
@@ -1,14 +1,68 @@
 #include "salarydialog.h"
 #include "ui_salarydialog.h"
+#include <QSqlQueryModel>
+#include <QSqlQuery>
 
 SalaryDialog::SalaryDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::SalaryDialog)
 {
     ui->setupUi(this);
+
+    QSqlQueryModel *qmodel=new QSqlQueryModel();
+    qmodel->setQuery("select pers.persname, perssalaryreg.perssalary from pers, perssalaryreg where pers.persid=perssalaryreg.persid and perssalaryreg.yearmonth = date_format(curdate(), '%Y%m')");
+    ui->tableView->setModel(qmodel);
+
+    QSqlQueryModel *qmodel1=new QSqlQueryModel();
+    qmodel1->setQuery("select projectname from project where date_format(finishdate, '%Y%m') = date_format(curdate(), '%Y%m')");
+    ui->comboBox->setModel(qmodel1);
+
 }
 
 SalaryDialog::~SalaryDialog()
 {
     delete ui;
+}
+
+void SalaryDialog::on_buttonBox_accepted()
+{
+
+}
+
+void SalaryDialog::on_toolBox_currentChanged()
+{
+    QSqlQueryModel *qmodel=new QSqlQueryModel();
+    qmodel->setQuery("select pers.persname, perssalaryreg.perssalary from pers, perssalaryreg where pers.persid=perssalaryreg.persid and perssalaryreg.yearmonth = date_format(curdate(), '%Y%m')");
+    ui->tableView->setModel(qmodel);
+}
+
+void SalaryDialog::on_pushButton_clicked()
+{
+    int projectbonus;
+    QString projectname;
+    projectname = ui->comboBox->currentText();
+
+//    QSqlQuery query_project;
+//    query_project.prepare("select projectid from project where projectname = ?");
+//    query_project.addBindValue(projectbonus);
+//    query_project.exec();
+//    query_project.next();
+//    project = query_project.value(0).toString();
+
+    projectbonus = ui->lineEdit->text().toInt();
+    QSqlQuery query;
+    query.prepare("update perssalaryreg set projectbonus = ? where persid = (select persid from project, workreg where project.projectname = ? and project.projectid = workreg.projectid)");
+    query.addBindValue(projectbonus);
+    query.addBindValue(projectname);
+    query.exec();
+}
+
+void SalaryDialog::on_pushButton_2_clicked()
+{
+    int festivalbonus;
+    festivalbonus = ui->lineEdit_2->text().toInt();
+    QSqlQuery query;
+    query.prepare("update perssalaryreg set festivalbonus = ? where yearmonth = date_format(curdate(), '%Y%m')");
+    query.addBindValue(festivalbonus);
+    query.exec();
 }
--- salarydialog.h
+++ salarydialog.h
@@ -15,6 +15,15 @@ public:
     explicit SalaryDialog(QWidget *parent = 0);
     ~SalaryDialog();
 
+private slots:
+    void on_buttonBox_accepted();
+
+    void on_toolBox_currentChanged();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::SalaryDialog *ui;
 };
